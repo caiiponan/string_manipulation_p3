ad878c5f90e3bb65d4b46a81a76cc9c5ceb2946b
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 18:57:28 +0800

    Pseudo code for program.

---
+++ not_using_index().py
@@ -0,0 +1,6 @@
+# Prompt user for input
+# Prompt user to input substring to find
+# Initialize default index to -1
+# Loop through the string to find the substring
+# Check if the substring is found
+# Print the index of the substring.
