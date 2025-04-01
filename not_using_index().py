dc251deec06411ce0039d93834192772634fba9d
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 19:00:03 +0800

    Let user input string and substring to find.

--- not_using_index().py
+++ not_using_index().py
@@ -1,5 +1,8 @@
 # Prompt user for input
+user_input = input("Enter a string: ")
 # Prompt user to input substring to find
+substring = input("Enter the substring to find: ")
+
 # Initialize default index to -1
 # Loop through the string to find the substring
 # Check if the substring is found
