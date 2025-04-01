393cf7e69fc637cdfcdff74dc8e1a3619253a83a
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 16:31:17 +0800

    Pseudo code for program.

---
+++ not_using_rstrip().py
@@ -0,0 +1,6 @@
+# Prompt user for input
+# Check if the string is empty
+# Iterate backwards through the string to find the last non-space character
+# Check for trailing spaces
+# Return the string without trailing spaces
+# Print the result without trailing spaces
