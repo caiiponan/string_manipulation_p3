509e6d9cec2a38b481669cc291629a2e77e128db
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 18:36:24 +0800

    Checks if string is as long as width given.

--- not_using_zfill().py
+++ not_using_zfill().py
@@ -4,5 +4,7 @@ user_input = input("Enter a string: ")
 width = int(input("Enter the width: "))
 
 # Check for padding
+padding_length = width - len(user_input)
+
 # If padding is needed, add spaces to the left to fit width given
 # Print result with added zeroes
