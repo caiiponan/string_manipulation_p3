ef1488821f765fa0843af4c2ca17faf6089f0338
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 17:36:45 +0800

    Initialize is_lower to true.

--- not_using_islower().py
+++ not_using_islower().py
@@ -1,6 +1,6 @@
-# Let user input a string
-user_input = input("Enter a string: ")
-# Initialize a boolean variable 'is_lowercase' to True.
+user_input = input("Enter a string: ") # Let user input a string
+is_lowercase = True # Initialize a boolean variable 'is_lowercase' to True.
+
 # Iterate over each character in user input.
 # Check if the character is a lowercase letter (between 'a' and 'z')
 # Print True if every character is lowercase
