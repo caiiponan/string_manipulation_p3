948677d76c610692de62aefdceefeba7fcb54bf0
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 17:42:23 +0800

    Check if conditions apply to the function.

--- not_using_islower().py
+++ not_using_islower().py
@@ -1,7 +1,9 @@
 user_input = input("Enter a string: ") # Let user input a string
 is_lowercase = True # Initialize a boolean variable 'is_lowercase' to True.
 
-# Iterate over each character in user input.
-# Check if the character is a lowercase letter (between 'a' and 'z')
-# Print True if every character is lowercase
-# Print False if any character is not lowercase
+for char in user_input: # Iterate over each character in user input.
+    if "A" <= char <= "Z": # Check if the character is an uppercase letter (between 'A' and 'Z')
+        is_lowercase = False # is_lowercase = False if any character is uppercase.
+        break
+    else:
+        is_lowercase = True # is_lowercase = True if every character is lowercase
