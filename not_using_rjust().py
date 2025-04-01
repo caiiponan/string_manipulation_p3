ca31eeb5cf864c15d4ac85c51532cd9662286732
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 18:30:29 +0800

    Let user input a string and width.

--- not_using_rjust().py
+++ not_using_rjust().py
@@ -1,5 +1,9 @@
 # Prompt user for input
+user_input = input("Enter a string: ")
+
 # Prompt user for width
+width = int(input("Enter the width: "))
+
 # Calculate how many characters to add
 # Add spaces to the left to fit width given
 # Print result with added spaces
