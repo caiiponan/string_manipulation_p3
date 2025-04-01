069425f5ed616e10c4d8a3e914b3a6b160ba0906
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 18:24:55 +0800

    Check if prefix is in string or not.

--- not_using_startswith().py
+++ not_using_startswith().py
@@ -8,6 +8,11 @@ prefix = input("Enter the prefix to check: ")
 starts_with = True
 
 # Loop trough each character in the prefix
+for i in range(len(prefix)):
+
 # Check if we reach the end of the text or if characters do not match
+    if i >= len(user_input) or user_input[i] != prefix[i]:
 # If the prefix is not found, set the result to False
+        starts_with = False
+        break
 # Print result
