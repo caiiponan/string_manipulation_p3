1ddd5b6187f6943ba7c4e34f3e91a6c2545e68c8
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 17:35:03 +0800

    Pseudo code for program.

---
+++ not_using_islower().py
@@ -0,0 +1,5 @@
+# Let user input a string
+# Iterate over each character in user input.
+# Check if the character is a lowercase letter (between 'a' and 'z')
+# Print True if every character is lowercase
+# Print False if any character is not lowercase
