41819cbf31d4c657ff280096b74729410c965594
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 17:01:13 +0800

    Ask user for suffix to remove.

--- not_using_removesuffix().py
+++ not_using_removesuffix().py
@@ -2,5 +2,7 @@
 user_input = input("Enter a string: ")
 
 # Ask user for suffix to remove
+remove_suffix = input("Enter the suffix to remove: ")
+
     # Check if the string ends with the specified suffix
 # Print result with removed suffix
