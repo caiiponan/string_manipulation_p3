f7dffbac96d73d3a66e529b4ca90d3d0cca5945b
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 17:01:47 +0800

    Check if string is ending with given suffix.

--- not_using_removesuffix().py
+++ not_using_removesuffix().py
@@ -4,5 +4,8 @@ user_input = input("Enter a string: ")
 # Ask user for suffix to remove
 remove_suffix = input("Enter the suffix to remove: ")
 
-    # Check if the string ends with the specified suffix
+# Check if the string ends with the specified suffix
+if user_input.endswith(remove_suffix):
+    result = user_input[:len(user_input) - len(remove_suffix)]
+
 # Print result with removed suffix
