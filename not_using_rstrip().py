7b2d618d32207f511240352f0713f39686aa5395
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 16:33:18 +0800

    Check if string is empty.

--- not_using_rstrip().py
+++ not_using_rstrip().py
@@ -2,6 +2,8 @@
 user_input = input("Enter a string: ")
 
 # Check if the string is empty
+index = len(user_input) - 1
+
 # Iterate backwards through the string to find the last non-space character
 # Check for trailing spaces
 # Return the string without trailing spaces
