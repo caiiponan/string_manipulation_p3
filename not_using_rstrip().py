e984daeae2a7f059ea13b66acf0a76b1f83a3f87
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 16:31:42 +0800

    Let user input a string.

--- not_using_rstrip().py
+++ not_using_rstrip().py
@@ -1,4 +1,6 @@
 # Prompt user for input
+user_input = input("Enter a string: ")
+
 # Check if the string is empty
 # Iterate backwards through the string to find the last non-space character
 # Check for trailing spaces
