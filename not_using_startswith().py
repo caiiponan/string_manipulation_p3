51af8a8dfaa559fd076070670b92f5c84007819b
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 18:23:25 +0800

    Assume boolean variable for prefix is true.

--- not_using_startswith().py
+++ not_using_startswith().py
@@ -3,7 +3,10 @@ user_input = input("Enter a string: ")
 
 # Let user input prefix to check
 prefix = input("Enter the prefix to check: ")
+
 # Assume string starts with prefix
+starts_with = True
+
 # Loop trough each character in the prefix
 # Check if we reach the end of the text or if characters do not match
 # If the prefix is not found, set the result to False
