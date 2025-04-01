5be969f0132d808c31d59aea08d04b5484a9f1dc
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 18:22:41 +0800

    Pseudo code for program.

---
+++ not_using_startswith().py
@@ -0,0 +1,7 @@
+# Prompt user for input
+# Let user input prefix to check
+# Assume string starts with prefix
+# Loop trough each character in the prefix
+# Check if we reach the end of the text or if characters do not match
+# If the prefix is not found, set the result to False
+# Print result
