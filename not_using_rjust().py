682aeacaae0b61d7f9c77b24bdfc17c9316f91bf
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 18:31:09 +0800

    Calculate spaces that will be added.

--- not_using_rjust().py
+++ not_using_rjust().py
@@ -5,5 +5,7 @@ user_input = input("Enter a string: ")
 width = int(input("Enter the width: "))
 
 # Calculate how many characters to add
+space_length = width - len(user_input)
+
 # Add spaces to the left to fit width given
 # Print result with added spaces
