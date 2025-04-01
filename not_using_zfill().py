85102a350d9f1f85dcfa82f9325e94bc7ca200f7
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 18:35:42 +0800

    Let user input a string and width.

--- not_using_zfill().py
+++ not_using_zfill().py
@@ -1,5 +1,8 @@
 # Prompt user for input
+user_input = input("Enter a string: ")
 # Prompt user for width
+width = int(input("Enter the width: "))
+
 # Check for padding
 # If padding is needed, add spaces to the left to fit width given
 # Print result with added zeroes
