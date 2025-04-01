5ad62fa9c7096477c990f1ec1f3d2d7c5b32d3de
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 17:00:48 +0800

    Let user input string.

--- not_using_removesuffix().py
+++ not_using_removesuffix().py
@@ -1,4 +1,6 @@
 # Prompt user for input
+user_input = input("Enter a string: ")
+
 # Ask user for suffix to remove
     # Check if the string ends with the specified suffix
 # Print result with removed suffix
