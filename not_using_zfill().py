76c1bad2fa1f4e3eef723625209b7b3219dd319c
caiiponan <carlosalberto.ponan.04@gmail.com>
2025-04-01 18:37:53 +0800

    Print output with added zeroes to the left.

--- not_using_zfill().py
+++ not_using_zfill().py
@@ -9,5 +9,6 @@ padding_length = width - len(user_input)
 # If padding is needed, add spaces to the left to fit width given
 if padding_length > 0:
     user_input = "0" * padding_length + user_input
-    
+
 # Print result with added zeroes
+print(user_input)
